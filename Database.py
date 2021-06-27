d55b76b8ff05057f90535131b04789bedea02aa5
ahhon <polygon2000x@gmail.com>
2021-06-27 17:01:19 +0700

    Database.restore()

--- Database.py
+++ Database.py
@@ -1,15 +1,58 @@
+from pathlib import Path
+import sys, os
+
 from EasyPipe import Pipe
 
 class Database:
-    def __init__(self, name=None):
+    def __init__(self, name=None, dbpath=None):
         self.name = name
+        self.dbpath = dbpath
 
     def dump(self):
         raise Exception(f'unimplemented')
 
+    def recreateFromSQL(self):
+        raise Exception(f'unimplemented')
+
+    def restore(self, fileSql, interactive=True):
+        fileSql = Path(fileSql).resolve()
+
+        if not fileSql.exists():
+            raise Exception("{fileSql} does not exist".format(fileSql=fileSql))
+
+        name = self.name
+        dbpath = self.dbpath
+        dbstr = f"{dbpath}" if dbpath else f"{name}"
+
+        if interactive:
+            resp = print(f"""
+Are you sure you want to restore database {name} from file {fileSql}?"
+"""[1:-1])
+
+            resp = input("[yes/no] ")
+
+            if resp not in ('y', 'yes'):
+                raise Exception('Aborted')
+
+        cat = {
+            '.xz': 'xzcat',
+            '.bz2': 'bzcat',
+            '.zip': 'zcat',
+            '.gz': 'zcat',
+        }.get(fileSql.suffix)
+
+        inp = ""
+        if cat:
+            p = Pipe([cat, str(fileSql)])
+            inp = p.stdout
+        else:
+            inp = fileSql.read_text()
+
+        self.recreateFromSQL(inp)
+
+
 class Database_sqlite(Database):
-    def __init__(self, *arg, dbpath=None, **kwarg):
-        self.dbpath = dbpath
+    def __init__(self, *arg, **kwarg):
         Database.__init__(self, *arg, **kwarg)
 
     def dump(self):
@@ -17,6 +60,17 @@ class Database_sqlite(Database):
         p = Pipe(["sqlite3", str(dbpath)], stdin=".dump\n")
         if p.status: raise Exception(f"sqlite3 failed (status = {p.status})")
         return p.stdout
+
+    def recreateFromSQL(self, inp):
+        dbpath = self.dbpath
+        if dbpath.exists(): dbpath.unlink()
+
+        p = Pipe(['sqlite3', str(dbpath)], stdin=inp)
+        if p.stderr:
+            sys.stderr.write("sqlite3:\n")
+            sys.stderr.write(p.stderr)
+        if p.status: raise Exception(f'sqlite error: status={p.status}')
+
 
 class Database_mysql(Database):
     def __init__(self, *arg, **kwarg):
